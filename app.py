de6aa183a382410a9759f58423258fa162107c62
KyathamRohith <Kyathamrohith76@gmail.com>
2026-03-11 18:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, Im rohith from hyderabad working in ust"
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
